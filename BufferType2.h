aa1a72d00cfc84bc32b16ea12df62c8464c331e8
Hal Incandenza <halincandenza@yandex.com>
2018-09-02 14:25:17 -0400

    Refactor and redesign buffers

--- BufferType2.h
+++ BufferType2.h
@@ -2,18 +2,16 @@
 #define RTSPROJECT_BUFFERTYPE2_H
 
 #include <utility>
-#include <mutex>
+#include <vector>
+#include <unistd.h>
 
-class BufferType2
-{
+class BufferType2 {
 public:
     BufferType2();
-    void write(char value, std::pair<int,int> location);
-    std::pair<int,int> read(char value);
+    void write(char value, std::pair<int, int> location);
+    std::pair<int, int> read(char value);
     void print(); //for testing purposes
 private:
-    int array[3][2]; //char array to store locations of values
+    std::vector<std::pair<int, int> > log {3};
 };
-
-
 #endif //RTSPROJECT_BUFFERTYPE2_H
