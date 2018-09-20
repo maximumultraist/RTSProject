170b5bbac8ee5723146c5ad28e0555467ed303cf
adam pellegrini <adampellegrini93@gmail.com>
2018-09-20 19:39:36 -0400

    added avoid_collisions

--- main.cpp
+++ main.cpp
@@ -13,8 +13,8 @@ BufferType1 bufferA;
 BufferType1 bufferB;
 BufferType2 bufferC;
 BufferType2 bufferD;
-std::mutex mtx1,mtx2;
-bool collision;
+std::mutex mtx1,mtx2;;
+bool collision, ready1, ready2, ready3;
 bool lockX, lockY, lockZ;
 
 void reset_locks()
@@ -24,10 +24,20 @@ void reset_locks()
     lockZ = false;
 }
 
+void reset_waits()
+{
+    ready1 = true;
+    ready2 = false;
+    ready3 = false;
+}
+
 void generate_positions () {
     for (int i = 1; i < 21; i++) {
+        while(!ready1){
+            //for synchronization
+        }
         if (i%2 == 1) {
-            std::lock_guard<std::mutex> lock(mtx1); // lock buffers
+            std::unique_lock<std::mutex> lock(mtx1); // lock buffers
             auto temp1 = bufferA.read('X'); // logic to generate new positions
             auto temp2 = bufferA.read('Y');
             auto temp3 = bufferA.read('Z');
@@ -40,7 +50,7 @@ void generate_positions () {
                 bufferB.write('Z', 3, (temp3.second + 1) % 7);
         }
         else {
-            std::lock_guard<std::mutex> lock(mtx1); // lock buffers
+            std::unique_lock<std::mutex> lock(mtx1);// lock buffers
             auto temp5 = bufferB.read('X'); // logic to generate new positions
             auto temp6 = bufferB.read('Y');
             auto temp7 = bufferB.read('Z');
@@ -53,32 +63,42 @@ void generate_positions () {
                 bufferA.write('Z', 3, (temp7.second + 1) % 7);
         }
         reset_locks();
+        ready1 = false;
+        ready2 = true;
         sleep(1);
     }
 }
 
 void record_positions () {
     for (int i = 1; i < 21; i++) {
+        while(!ready2) {
+            //for synchronization
+        }
         if (i%2 == 1) {
-            std::lock_guard<std::mutex> lock(mtx2);
+            std::unique_lock<std::mutex> lock(mtx2);
             bufferC.write('X', bufferA.read('X'));
             bufferC.write('Y', bufferA.read('Y'));
             bufferC.write('Z', bufferA.read('Z'));
 
         }
         else {
-            std::lock_guard<std::mutex> lock(mtx2);
+            std::unique_lock<std::mutex> lock(mtx2);
             bufferD.write('X', bufferB.read('X'));
             bufferD.write('Y', bufferB.read('Y'));
             bufferD.write('Z', bufferB.read('Z'));
         }
+        ready2 = false;
+        ready3 = true;
         sleep(1);
     }
 }
 
 void avoid_collisions () {
-    for(int i = 2; i < 22; i++) {
-        if(i%2 != 1) {
+    for(int i = 1; i < 21; i++) {
+        while(!ready3){
+            //for synchronization
+        }
+        if(i%2 == 1) {
             //pulling initial location of values
             std::pair pairX = bufferC.read('X');
             std::pair pairY = bufferC.read('Y');
@@ -102,7 +122,6 @@ void avoid_collisions () {
                     lockX = true;
                     std::cout<<"Collision predicted and avoided between X and Y at second "<<i+2<<std::endl;
                 }
-
             }
 
             else if(pairX.first == pairZ.first && pairX.second == pairZ.second) { //if X and Z collide 2 moves ahead
@@ -130,7 +149,7 @@ void avoid_collisions () {
             }
 
             if(pairX.first == pairY.first && pairX.second == pairY.second) { //if X and Y collide 2 moves ahead
-                if(pairY.first == pairZ.first && pairY.second == pairZ.second) { //if X, Y, and Z al collide 2 moves ahead
+                if(pairY.first == pairZ.first && pairY.second == pairZ.second) { //if X, Y, and Z all collide 2 moves ahead
                     lockX = true;
                     lockY = true;
                     std::cout<<"Collision predicted and avoided between X, Y, and Z at second "<<i+2<<std::endl;
@@ -151,14 +170,18 @@ void avoid_collisions () {
                 std::cout<<"Collision predicted and avoided between Y and Z at second "<<i+2<<std::endl;
             }
         }
+        ready3 = false;
+        ready1 = true;
         sleep(1);
     }
-
 }
 
 void detect_collisions () {
     sleep(1);
     for (int i = 2; i < 21; i++) {
+        while(!ready3){
+
+        }
         collision = false;
         if (i%2 != 1) {
             if (bufferC.read('X') == bufferC.read('Y')) {
@@ -199,12 +222,15 @@ void detect_collisions () {
         }
         if ( collision == false)
             std::cout<<"No collision detected at second: "<<i<<"\n";
+        ready3 = false;
+        ready1 = true;
         sleep(1);
     }
 }
 
 int main(void) {
     reset_locks();
+    reset_waits();
     BufferType1 bufferA;
     BufferType1 bufferB;
     BufferType2 bufferC;
